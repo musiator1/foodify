cc78f826f1ef1c4c8b944c6a8c08031348655a0c
Rafał Musiatowicz <rafalmusiatowicz2@gmail.com>
2026-08-05 21:54:25 +0200

    Dodanie Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
-// Dodaj to pod importami w main.tsx
+// Ustawienia motywu
 const savedTheme = localStorage.getItem('theme') || 'zinc';
 document.documentElement.setAttribute('data-theme', savedTheme);
 
@@ -14,6 +15,7 @@ if (isDark) document.documentElement.classList.add('dark');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
 
